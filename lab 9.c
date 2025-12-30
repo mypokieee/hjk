cc5026308a495c15a5c275d6a49b9f1caf09fe1b
unknown <lab2@bf02pc36.ssuet.edu.pk>
2025-12-30 15:03:08 +0500

    Added lab 9

---
+++ lab 9.c
@@ -0,0 +1,15 @@
+///write a function using c language that display general instruction before attempting an online exam:
+///return type
+#include<stdio.h>
+void g_instruction(void);
+int main(){
+	g_instruction();
+	
+	return 0;
+}
+void g_intruction(void){
+	printf("folllow the particular instruction:\n");
+	printf("1) Do not close the tab.\n");
+	printf("2) wifi is important do not close it. \n");
+	printf("3) using Google or Any AI is strictly prohibited.\n");
+}
